1b7bdd9357723ea515280da9c0da028ac8816763
Fathur Rahman <fathur00013@gamil.com>
2023-12-26 00:10:59 +0700

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,3 @@
-// console.log('client side javascript is loaded')
-
-// fetch('https://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
 
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
